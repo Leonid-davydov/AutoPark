a079bdc2b9e2564b0d7979752604bac7f946a4d9
Leonid-davydov <lenya-davydov@mail.ru>
2022-04-28 17:54:19 +0300

    level 9

--- src/Main.java
+++ src/Main.java
@@ -4,157 +4,26 @@ import java.util.Comparator;
 public class Main {
     public static void main(String[] args) {
 
-//        VehicleType[] types = new VehicleType[]{new VehicleType("Bus", 1.2d),
-//                new VehicleType("Car", 1.0d), new VehicleType("Rink", 1.5d),
-//                new VehicleType("Tractor", 1.2d)};
-//
-//        types[3].setCoefficient(1.3d);
-//
-//        double maxCoef = types[0].getCoefficient();
-//        double sumCoef = 0.0d;
-//        double avrCoef;
-//
-//        for (VehicleType type : types) {
-//            type.display();
-//            if (type.getCoefficient() > maxCoef) {
-//                maxCoef = type.getCoefficient();
-//            }
-//            sumCoef += type.getCoefficient();
-//        }
-//
-//        avrCoef = sumCoef / types.length;
-//        System.out.println(maxCoef + " " + avrCoef);
-//
-//
-//        Vehicle[] vehicles = new Vehicle[]{
-//                new Vehicle(types[0], "Volkswagen Crafter", "5427 AX-7", 2022,
-//                        2015, 376000, Color.Blue,
-//                        new GasolineEngine(2, 8.1, 75)),
-//                new Vehicle(types[0], "Volkswagen Crafter", "6427 AA-7", 2500,
-//                        2014, 227010, Color.White,
-//                        new GasolineEngine(2.18, 8.5, 75)),
-//                new Vehicle(types[0], "Electric Bus E321", "6785 BA-7", 12080,
-//                        2019, 20451, Color.Green,
-//                        new ElectricalEngine(50, 150)),
-//                new Vehicle(types[1], "Golf 5", "8682 AX-7", 1200,
-//                        2006, 230451, Color.Gray,
-//                        new DieselEngine(1.6, 7.2, 55)),
-//                new Vehicle(types[1], "Tesla Model S 70D", "0001 AA-7", 2200,
-//                        2019, 10454, Color.White,
-//                        new ElectricalEngine(25, 70)),
-//                new Vehicle(types[2], "Hamm HD 12VV", null,
-//                        3000, 2016, 122, Color.Yellow,
-//                        new DieselEngine(3.2, 25, 20)),
-//                new Vehicle(types[3], "МТЗ Беларус-1025.4", "1145 AB-7", 1200,
-//                        2020, 109, Color.Red,
-//                        new DieselEngine(4.75, 20.1, 135))
-//        };
-//
-//        System.out.println();
-//        Display.showArray(vehicles);
-/*
-        Vehicle copy;
-
-        for (int i = 0; i < vehicles.length; i++) {
-            for (int j = 0; j < vehicles.length - 1; j++) {
-                if (vehicles[j].compareTo(vehicles[j + 1]) > 0) {
-                    copy = vehicles[j + 1];
-                    vehicles[j + 1] = vehicles[j];
-                    vehicles[j] = copy;
-                }
-            }
-        }
-
-        System.out.println();
-
-        Display.showArray(vehicles);
-
-        Vehicle maxMileage = vehicles[0];
-        Vehicle minMileage = vehicles[0];
-        for (Vehicle vehicle : vehicles) {
-            if (vehicle.getMileage() > maxMileage.getMileage()) {
-                maxMileage = vehicle;
-            }
-            if (vehicle.getMileage() < minMileage.getMileage()) {
-                minMileage = vehicle;
-            }
-        }
-
-        System.out.println("Car with max mileage: " + maxMileage);
-        System.out.println("Car with min mileage: " + minMileage);
-*/
-
-//        for (int i = 0; i < vehicles.length; i++) {
-//            for (int j = i; j < vehicles.length - 1; j++) {
-//                if (vehicles[j].equals(vehicles[j + 1])) {
-//                    System.out.println("Одинаковые машины:");
-//                    System.out.println(vehicles[j].toString());
-//                    System.out.println(vehicles[j + 1].toString());
-//                }
-//            }
-//        }
-//
-//        Vehicle maxRange = vehicles[0];
-//
-//        for (Vehicle vehicle : vehicles) {
-//            if (vehicle.engine.getMaxKilometers() > maxRange.engine.getMaxKilometers()) {
-//                maxRange = vehicle;
-//            }
-//        }
-//        System.out.println("машина которая проедет максимальное расстояние");
-//        System.out.println(maxRange.toString());
-
-/*        VehicleCollection vc = new VehicleCollection("types", "vehicles", "rents");
-        vc.display();
-        Vehicle v = new Vehicle(vc.VTCollection.get(0), "Volkswagen Crafter", "5427 AX-7", 2022,
-                        2015, 376000, Color.Blue,
-                        new GasolineEngine(2, 8.1, 75));
-        vc.insert(7, v);
-        vc.delete(1);
-        vc.delete(4);
-        vc.display();
-        Comparator<Vehicle> comparator = new Comparator<Vehicle> () {
-            @Override
-            public int compare(Vehicle v1, Vehicle v2) {
-                return Integer.compare(v1.getId(), v2.getId());
-            }
-        };
-        vc.sort(comparator);
-        vc.display();
-*/
-
-//        VehicleCollection vc = new VehicleCollection("types", "vehicles", "rents");
-//        WashQueue wq = new WashQueue();
-//
-//        for (Vehicle v : vc.VCollection) {
-//            wq.enqueue(v);
-//        }
-//
-//        int number = 0;
-//
-//        while (wq.size() != 0) {
-//            wq.dequeue();
-//            number++;
-//            System.out.println("Auto " + number + " вымыто");
-//        }
-
         VehicleCollection vc = new VehicleCollection("types", "vehicles", "rents");
-        Stack garage = new Stack();
-
-        int number = 0;
+        MechanicService mechanicService = new MechanicService();
 
         for (Vehicle v : vc.VCollection) {
-            garage.push(v);
-            number++;
-            System.out.println("Auto " + number + " заехало в гараж");
+            if (mechanicService.detectAndRepair(v))
+                System.out.println(v);
         }
 
-        System.out.println("Гараж заполнен");
+        for (Vehicle v : vc.VCollection) {
+            mechanicService.detectBreaking(v);
+        }
 
-        while (garage.size() != 0){
-            garage.pop();
-            System.out.println("Auto " + number + " выехало из гаража");
-            number--;
+        int max = mechanicService.findMost();
+        System.out.println("Id max = " + max);
+
+        for (Vehicle v : vc.VCollection) {
+            if (v.getId() == max){
+                System.out.println(v);
+                break;
+            }
         }
     }
 
---
+++ src/MechanicService.java
@@ -0,0 +1,148 @@
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MechanicService implements Fixer{
+    private static String[] details = new String[] {"Filter", "Sleeve", "Shaft", "Axis", "Candle", "Oil", "GRM", "ShRUS"};
+    private static String PATH = "./breakdowns.csv";
+    private static final CSVParser parserColon = new CSVParserBuilder()
+            .withSeparator(',')
+            .withIgnoreQuotations(true)
+            .build();
+
+    @Override
+    public Map<String, Integer> detectBreaking(Vehicle vehicle) {
+
+            Map<String, Integer> breakdowns = new HashMap<>();
+            int id = vehicle.getId();
+            String line = "" + id;
+            String detail;
+            Integer number;
+
+            for (int i = 0; i < 3; i++){
+                detail = details[getRandomInteger(0,7)];
+                number = getRandomInteger(0,2);
+
+                if (number != 0) {
+                    breakdowns.put(detail, number);
+                }
+            }
+
+            for(Map.Entry<String, Integer> entry : breakdowns.entrySet()){
+                line += "," + entry.getKey() + "," + entry.getValue();
+            }
+
+            if (!breakdowns.isEmpty())
+                write(line);
+
+        return breakdowns;
+    }
+
+    @Override
+    public void repair(Vehicle vehicle) {
+        List<String[]> lines = null;
+
+        try (CSVReader reader = new CSVReader(new FileReader(PATH))) {
+
+            lines = reader.readAll();
+
+            for (int i = 1; i < lines.size(); i++) {
+                if (Integer.parseInt(lines.get(i)[0].trim()) == vehicle.getId())
+                    lines.remove(i);
+            }
+
+            CSVWriter writer = new CSVWriter(new FileWriter(PATH),
+                    ',',
+                    CSVWriter.NO_QUOTE_CHARACTER,
+                    ' ',
+                    "\n");
+
+            writer.writeAll(lines);
+            writer.close();
+
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean isBroken(Vehicle vehicle) {
+        List<String[]> lines;
+
+        try (CSVReader reader = new CSVReader(new FileReader(PATH))) {
+
+            lines = reader.readAll();
+
+            for (int i = 1; i < lines.size(); i++) {
+                if (Integer.parseInt(lines.get(i)[0].trim()) == vehicle.getId())
+                    return true;
+            }
+
+
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    private static int getRandomInteger(int min, int max) {
+        return (int) ((Math.random() * ((max - min) + 1)) + min);
+    }
+
+    private void write(String str) {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(PATH, true),
+                ',',
+                CSVWriter.NO_QUOTE_CHARACTER,
+                ' ',
+                "\n"))
+        {
+            String[] line = {str};
+            writer.writeNext(line, false);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public int findMost() {
+        int id = 0;
+        int maxBreakdowns = 0;
+        int sum;
+        List<String[]> lines;
+
+        try (CSVReader reader = new CSVReader(new FileReader(PATH))) {
+
+            lines = reader.readAll();
+
+            for (int i = 1; i < lines.size(); i++) {
+                sum = 0;
+
+                for (int j = 2; j < lines.get(i).length; j +=2){
+                    sum += Integer.parseInt(lines.get(i)[j].trim());
+                }
+
+                if (sum > maxBreakdowns) {
+                    maxBreakdowns = sum;
+                    id = Integer.parseInt(lines.get(i)[0].trim());
+                }
+            }
+
+            System.out.println("Max number of Breakdowns is " + maxBreakdowns);
+
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+
+        return id;
+    }
+}
